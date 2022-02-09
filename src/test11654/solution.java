992cd880b76597a0b9a7953b5e3957758ef84a48
eonhwakim <eonhwa97@gmail.com>
2022-02-09 09:37:49 +0900

    BOJ 11654 아스키 코드

---
+++ src/test11654/solution.java
@@ -0,0 +1,19 @@
+package test11654;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int in = sc.next().charAt(0);
+        System.out.println(in);
+
+
+    }
+//    public static void main(String[] args) throws IOException {
+//        int in = System.in.read();
+//        System.out.println(in);
+//    }
+
+}
