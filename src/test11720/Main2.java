0cda04632ca1eeec6b2f1b1f34e049f1ec0a0628
eonhwakim <eonhwa97@gmail.com>
2022-02-09 13:53:55 +0900

    BOJ 11720 숫자의 합

---
+++ src/test11720/Main2.java
@@ -0,0 +1,19 @@
+package test11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int in = Integer.parseInt(br.readLine());
+        String[] num = br.readLine().split("");
+
+        int sum = 0;
+        for(int i=0; i<in; i++){
+            sum += Integer.parseInt(num[i]);
+        }
+        System.out.println(sum);
+    }
+}
