f06ff83ef0345ff11c9a1d33e96dc119e8397aa8
eonhwakim <eonhwa97@gmail.com>
2022-02-20 00:18:44 +0900

    BOJ 14425 문자열 집합

---
+++ src/test14425/Main.java
@@ -0,0 +1,31 @@
+package test14425;
+//HashMap 사용
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int num = 0;
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int i =0; i<n; i++){
+            set.add(br.readLine());
+        }
+
+        for(int i=0; i<m; i++){
+            String str = br.readLine();
+            if(set.contains(str)) num++;
+        }
+        System.out.println(num);
+    }
+}
